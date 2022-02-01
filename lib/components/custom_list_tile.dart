35e054b638e8f2e318d1ce606d298ff14e742e49
Vitor Dutra <jo.vitor.d@gmail.com>
2022-02-01 15:56:58 -0300

    Finish custom tile layout

--- lib/components/custom_list_tile.dart
+++ lib/components/custom_list_tile.dart
@@ -20,20 +20,27 @@ Widget CustomListTile(Article article) {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
+          height: 200.0,
+          width: double.infinity,
           decoration: BoxDecoration(
             image: DecorationImage(
               image: NetworkImage(article.urlToImage),
+              fit: BoxFit.cover,
             ),
             borderRadius: BorderRadius.circular(8.0),
           ),
         ),
         SizedBox(height: 8.0),
         Container(
+          padding: EdgeInsets.all(8.0),
           decoration: BoxDecoration(
             color: Colors.red,
             borderRadius: BorderRadius.circular(30.0),
           ),
-          child: Text(article.source.name),
+          child: Text(
+            article.source.name,
+            style: TextStyle(color: Colors.white),
+          ),
         ),
         SizedBox(height: 8.0),
         Text(
