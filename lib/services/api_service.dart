61b069d7e0d3d9e609ac3cac3bd19924227e24af
Alexandre Aragão <alexr.aragao@gmail.com>
2022-02-03 09:18:05 -0300

    bugfix: Remover noticias sem imagem

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -13,7 +13,7 @@ class ApiService {
       Map<String, dynamic> json = jsonDecode(res.body);
       List<dynamic> body = json['articles'];
       List<Article> articles =
-          body.map((dynamic item) => Article.fromJson(item)).toList();
+          body.map((dynamic item) => Article.fromJson(item)).where((element) => element.urlToImage != null).toList();
       return articles;
     } else {
       throw ("Can't get the Articles");
